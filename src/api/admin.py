8577d82184f8c19104ff7515f7e81dd24be3d304
Bibek Acharya <71765138+bibekach@users.noreply.github.com>
2023-05-18 21:23:12 +0000

    Data model created

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,17 +1,27 @@
-  
+
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, MemberAccount, UserProfile, ProviderProfile, Address, ServiceRequest, Notification, ProviderAvailability, Exclusion, ServiceList
 from flask_admin.contrib.sqla import ModelView
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(MemberAccount, db.session))
+    admin.add_view(ModelView(UserProfile, db.session))
+    admin.add_view(ModelView(ProviderProfile, db.session))
+    admin.add_view(ModelView(Address, db.session))
+    admin.add_view(ModelView(ServiceRequest, db.session))
+    admin.add_view(ModelView(Notification, db.session))
+    admin.add_view(ModelView(Exclusion, db.session))
+    admin.add_view(ModelView(ProviderAvailability, db.session))
+    admin.add_view(ModelView(ServiceList, db.session))
 
-    # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
+
+# You can duplicate that line to add mew models
+# admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -17,3 +18,108 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class MemberAccount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    account_type = db.Column(db.Integer, nullable=False)
+    user_or_provider_id = db.Column(db.Integer, nullable=False)
+    member_name = db.Column(db.String(100), unique=True, nullable=True)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    user_profile = db.relationship('UserProfile', backref='member_account')
+    provider_profile = db.relationship(
+        'ProviderProfile', backref='member_account')
+    addresses = db.relationship('Address', backref='member_account')
+
+
+class UserProfile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('member_account.id'))
+    must_have_certificate = db.Column(db.Boolean(), nullable=False)
+    required_experience = db.Column(db.Integer, nullable=False)
+    required_rating = db.Column(db.Float)
+    # exclusion_csv_string = db.Column(db.String, nullable=True)
+    avatar_images = db.Column(db.String, nullable=True)
+    service_requests = db.relationship(
+        'ServiceRequest', backref='user_profile')
+    notifications = db.relationship('Notification', backref='user_profile')
+    exclusion = db.relationship('Exclusion', backref='user_profile')
+
+
+class ProviderProfile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('member_account.id'))
+    has_certificate = db.Column(db.Boolean())
+    experience = db.Column(db.Integer, nullable=False)
+    # services_csv_string = db.Column(db.String(200), nullable=False)
+    service_radius = db.Column(db.Integer, nullable=False)
+    average_rating = db.Column(db.Float)
+    ratings_counter = db.Column(db.Integer, nullable=False)
+    member_id = db.Column(db.Integer, db.ForeignKey('member_account.id'))
+    avatar_images = db.Column(db.String, nullable=True)
+    service_requests = db.relationship(
+        'ServiceRequest', backref='provider_profile')
+    notifications = db.relationship('Notification', backref='provider_profile')
+    service_list = db.relationship('ServiceList', backref='provider_profile')
+    exclusion = db.relationship('Exclusion', backref='provider_profile')
+
+
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    is_main = db.Column(db.Boolean())
+    user_id = db.Column(db.Integer, db.ForeignKey('member_account.id'))
+    street = db.Column(db.String(100), nullable=True)
+    apartment = db.Column(db.String(100), nullable=True)
+    city = db.Column(db.String(100), nullable=True)
+    state = db.Column(db.String(100), nullable=True)
+    postal_code = db.Column(db.String(100), nullable=True)
+    country = db.Column(db.String(100), nullable=True)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
+
+class ServiceRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    time_slot = db.Column(db.Integer)
+    recurrence = db.Column(db.Integer)
+    service = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user_profile.id'), nullable=True)
+    provider_id = db.Column(db.Integer, db.ForeignKey(
+        'provider_profile.id'), nullable=True)
+    address_id = db.Column(db.Integer, db.ForeignKey(
+        'address.id'), nullable=False)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type_of_notification = db.Column(db.Integer)
+    status = db.Column(db.Integer)
+    publishing_date_time = db.Column(db.DateTime, nullable=False)
+    message = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user_profile.id'), nullable=True)
+    provider_id = db.Column(db.Integer, db.ForeignKey(
+        'provider_profile.id'), nullable=True)
+
+
+class Exclusion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_profile.id'))
+    provider_id = db.Column(db.Integer, db.ForeignKey('provider_profile.id'))
+
+
+class ProviderAvailability(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String(100), nullable=False)
+    time_slot = db.Column(db.String(100), nullable=False)
+
+
+class ServiceList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    provider_id = db.Column(db.Integer, db.ForeignKey(
+        'provider_profile.id'), nullable=False)
+    services_name = db.Column(db.String(100), nullable=False)
