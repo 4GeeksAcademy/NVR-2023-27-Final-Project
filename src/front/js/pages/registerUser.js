45209933e82ba603b1e350ac334f24ae54a0c17c
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-05-20 15:47:39 +0000

    Credentials and Address form started

--- src/front/js/pages/registerUser.js
+++ src/front/js/pages/registerUser.js
@@ -5,7 +5,30 @@ import "../../styles/home.css";
 
 export const RegisterUser = () => {
     const { store, actions } = useContext(Context);
+    const [userCredentials, setUserCredentials] = useState({
+        name: "",
+        email: "",
+        password: "",
+        must_have_certificate: true,
+        required_experience: 1,
+        required_rating: 3,
+        avatar_image: "",
+    })
+
+    const [userAddress, setUserAddress] = useState({
+        street: "",
+        apartment: "",
+        city: "",
+        state: "",
+        postalCode: "",
+        country: "",
+        lattitude: 0,
+        longitude: 0,
+
+    });
+
     const navigate = useNavigate();
+
     const handleClickHome = () => {
         navigate("/");
     }
@@ -13,16 +36,156 @@ export const RegisterUser = () => {
         navigate("/privateUser");
     }
 
+    const handleChangeCredentials = (event) => {
+        const { name, value } = event.target;
+        setUserCredentials((previousCredentials) => ({ ...previousCredentials, [name]: value }));
+    };
+
+    const handleChangeAddress = (event) => {
+        const { name, value } = event.target;
+        setUserAddress((previousAddress) => ({ ...previousAddress, [name]: value }));
+    };
+  
+    const handleSubmitCredentials = (event) => {
+        event.preventDefault();
+        alert("Submit credentials");
+    };
+
+    const handleSubmitAddress = (event) => {
+        event.preventDefault();
+        alert("Submit Address");
+
+    };
 
     return (
         <>
             <p>Register user</p>
             <button className="btn btn-primary mx-3" onClick={handleClickHome}>
-            Home
+                Home
             </button>
             <button className="btn btn-success mx-3" onClick={handleClickPrivate}>
-            Private
+                Private
             </button>
+            {/* Credentials Form */}
+            <div>
+                <form className="w-50 mx-auto" onSubmit={handleSubmitCredentials}>
+                    <div className="mb-3">
+                        <label className="form-label">Name</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="name"
+                            value={userCredentials.name}
+                            onChange={handleChangeCredentials}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Emall</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="email"
+                            value={userCredentials.email}
+                            onChange={handleChangeCredentials}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Password</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="password"
+                            value={userCredentials.password}
+                            onChange={handleChangeCredentials}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <button type="submit" className="btn btn-primary" >
+                            Submit Credentials
+                        </button>
+                    </div>
+                </form>
+                <div>
+                    <h2>Credentials:</h2>
+                    <pre>{JSON.stringify(userCredentials, null, 2)}</pre>
+                </div>
+            </div>
+            {/* Main Address Form */}
+            <div>
+                <form className="w-50 mx-auto" onSubmit={handleSubmitAddress}>
+                    <div className="mb-3">
+                        <label className="form-label">Street</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="street"
+                            value={userAddress.street}
+                            onChange={handleChangeAddress}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Apartment</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="apartment"
+                            value={userAddress.apartment}
+                            onChange={handleChangeAddress}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">City</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="city"
+                            value={userAddress.city}
+                            onChange={handleChangeAddress}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">State</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="state"
+                            value={userAddress.state}
+                            onChange={handleChangeAddress}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Postal Code</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="postalCode"
+                            value={userAddress.postalCode}
+                            onChange={handleChangeAddress}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Country</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="country"
+                            value={userAddress.country}
+                            onChange={handleChangeAddress}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <button type="submit" className="btn btn-primary" >
+                            Submit Address
+                        </button>
+                    </div>
+                </form>
+                <div>
+                    <h2>Address:</h2>
+                    <pre>{JSON.stringify(userAddress, null, 2)}</pre>
+                </div>
+            </div>
+
+
         </>
     );
 };
