3a4a81b274cac81b91a50e117142b809ddb3651e
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-05-28 20:42:38 +0000

    few chnages -  28/05/2023

--- src/front/js/pages/privateUser.js
+++ src/front/js/pages/privateUser.js
@@ -17,23 +17,60 @@ export const PrivateUser = () => {
 
   return (
     <>
-      <p>Private user</p>
-      <button className="btn btn-primary mx-3" onClick={handleClickHome}>
-        Home
-      </button>
+      <nav className="navbar bg-light fixed-top">
+        <div className="container-fluid">
+          <button className="btn btn-primary" onClick={(handleClickHome)}>
+            Home
+          </button>
+          <div className="dropdown">
+            <button
+              className="bg-light border-0"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Category
+            </button>
+            <ul className="dropdown-menu rounded-0">
+              <li>
+                  Action
+              </li>
+              <li>
+                  Another action
+              </li>
+              <li>
+                Something else here
+              </li>
+            </ul>
+          </div>
+
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+
+          </form>
+        </div>
+      </nav>
 
       {/* Display service descriptions */}
-      <div>
+      <div className="py-5">
         {store.serviceDescriptions.map((description, index) => (
           <div key={index}>
-            <h3>{description.category}</h3>
-            <p>{description.service}</p>
-            <p>{description.description}</p>
-            <p>{description.unit}</p>
-            <p>{description.duration}</p>
-            <p>{description.personnel}</p>
-            <p>{description.included}</p>
-            <p>{description.price}</p>
+            <h3>Category: {description.category}</h3>
+            <p>Service: {description.service}</p>
+            <p>Description: {description.description}</p>
+            <p>Unit: {description.unit}</p>
+            <p>Duration: {description.duration}</p>
+            <p>Personnel: {description.personnel}</p>
+            <p>Includes: {description.included}</p>
+            <p>Price: {description.price}</p>
           </div>
         ))}
       </div>
