50cf97243a2161eefa5891855eee3602c0bc321c
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-06-04 12:24:00 +0000

    dropdown 2 04/06/2023

--- src/front/js/pages/privateUser.js
+++ src/front/js/pages/privateUser.js
@@ -104,26 +104,27 @@ export const PrivateUser = () => {
             <div className="container-fluid mx-0 px-0 gx-0">
                 <header>
                     <nav className="navbar fixed-top px-3 firstBavBar">
-                            <button onClick={() => handleSectionClick("home")}>Home</button>
-                            <button
-                                className={`menuLabel ${selectedSection === "requestService" ? "selectedSectionButton active" : ""}`}
-                                onClick={() => handleSectionClick("requestService")}
-                            ><span>request service</span></button>
-                            <button
-                                className={`menuLabel ${selectedSection === "myRequests" ? "selectedSectionButton active" : ""}`}
-                                onClick={() => handleSectionClick("myRequests")}
-                            ><span>my requests</span></button>
-                            <button
-                                className={`menuLabel ${selectedSection === "notifications" ? "selectedSectionButton active" : ""}`}
-                                onClick={() => handleSectionClick("notifications")}
-                            ><span>notifications</span></button>
-                            <button onClick={handleSignout}>Sign Out</button>
-                            <button>Avatar</button>
+                        <button onClick={() => handleSectionClick("home")}>Home</button>
+                        <button
+                            className={`menuLabel ${selectedSection === "requestService" ? "selectedSectionButton active" : ""}`}
+                            onClick={() => handleSectionClick("requestService")}
+                        ><span>request service</span></button>
+                        <button
+                            className={`menuLabel ${selectedSection === "myRequests" ? "selectedSectionButton active" : ""}`}
+                            onClick={() => handleSectionClick("myRequests")}
+                        ><span>my requests</span></button>
+                        <button
+                            className={`menuLabel ${selectedSection === "notifications" ? "selectedSectionButton active" : ""}`}
+                            onClick={() => handleSectionClick("notifications")}
+                        ><span>notifications</span></button>
+                        <button onClick={handleSignout}>Sign Out</button>
+                        <button>Avatar</button>
                     </nav>
                     {selectedSection === "requestService" && (
                         <nav className="navbar fixed-top secondNavBar d-flex justify-content-center align-items-center ">
                             <div className="d-flex justify-content-center align-items-center">
-                                <div className="dropdown ribbonElement1">
+                                {/* Category Dropdown*/}
+                               <div className="dropdown ribbonElement1">
                                     <button
                                         className="border-0"
                                         type="button"
@@ -131,13 +132,13 @@ export const PrivateUser = () => {
                                         aria-expanded="false"
                                     >
                                         <span className="dropDownIcon1">
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M384-264v-72h192v72H384ZM240-444v-72h480v72H240Zm-96-180v-72h672v72H144Z"/></svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M384-264v-72h192v72H384ZM240-444v-72h480v72H240Zm-96-180v-72h672v72H144Z" /></svg>
                                         </span>
-                                        <span className="pullDownLabel me-1">Category:</span>
-                                        <span className="pullDownLabel italic ms-1">{selectedCategory}</span>
+                                        <span className="pullDownLabel me-1">Category</span>
                                         <span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M480-384 288-576h384L480-384Z"/></svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M480-384 288-576h384L480-384Z" /></svg>
                                         </span>
+                                        <span className="pullDownLabel italic ms-1">{selectedCategory}</span>
                                     </button>
                                     <ul className="dropdown-menu rounded-0">
                                         {categories.map((category, index) => (
@@ -156,13 +157,13 @@ export const PrivateUser = () => {
                                         aria-expanded="false"
                                     >
                                         <span className="dropDownIcon2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M192-96v-72h576v72H192Zm288-144L336-384l51-51 57 57v-204l-57 57-51-51 144-144 144 144-51 51-57-57v204l57-57 51 51-144 144ZM192-792v-72h576v72H192Z"/></svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M192-96v-72h576v72H192Zm288-144L336-384l51-51 57 57v-204l-57 57-51-51 144-144 144 144-51 51-57-57v204l57-57 51 51-144 144ZM192-792v-72h576v72H192Z" /></svg>
                                         </span>
-                                        <span className="pullDownLabel me-1">Price:</span>
-                                        <span className="pullDownLabel italic ms-1">{selectedPrice}</span>
+                                        <span className="pullDownLabel me-1">Price</span>
                                         <span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M480-384 288-576h384L480-384Z"/></svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M480-384 288-576h384L480-384Z" /></svg>
                                         </span>
+                                        <span className="pullDownLabel italic ms-1">{selectedPrice}</span>
                                     </button>
                                     <ul className="dropdown-menu rounded-0">
                                         {prices.map((price, index) => (
@@ -175,10 +176,10 @@ export const PrivateUser = () => {
                                 {/* Search Bar*/}
                                 <div className="ribbonElement3">
                                     <form role="search">
-                                        <span className="menuIcon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M765-144 526-383q-30 22-65.792 34.5T384.035-336Q284-336 214-406t-70-170q0-100 70-170t170-70q100 0 170 70t70 170.035q0 40.381-12.5 76.173T577-434l239 239-51 51ZM384-408q70 0 119-49t49-119q0-70-49-119t-119-49q-70 0-119 49t-49 119q0 70 49 119t119 49Z"/></svg>
+                                        <span className="menuIcon3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M765-144 526-383q-30 22-65.792 34.5T384.035-336Q284-336 214-406t-70-170q0-100 70-170t170-70q100 0 170 70t70 170.035q0 40.381-12.5 76.173T577-434l239 239-51 51ZM384-408q70 0 119-49t49-119q0-70-49-119t-119-49q-70 0-119 49t-49 119q0 70 49 119t119 49Z" /></svg>
                                         </span>
-                                       <label htmlFor="searchField" className="pullDownLabel me-1">Find:</label>
+                                        <label htmlFor="searchField" className="pullDownLabel me-1">Find:</label>
                                         <input
                                             id="searchField"
                                             type="search"
