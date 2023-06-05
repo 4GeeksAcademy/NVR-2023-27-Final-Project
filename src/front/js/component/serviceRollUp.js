f0ab67041458c5a8653005633c238ca888930cc0
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-06-05 20:54:51 +0000

    expandable 05/96/2023

--- src/front/js/component/serviceRollUp.js
+++ src/front/js/component/serviceRollUp.js
@@ -1,19 +1,48 @@
 import React from "react";
 
 export const ServiceRollUp = (props) => {
-    const { id, category, service, description, price, } = props.serviceObject;
+    const { id, category, service, description, price } = props.serviceObject;
+
 
 
     return (
-        <>
-            <div className="serviceRollUp">
-                <div className="serviceRollUpCategoryLabelWrapper">
-                    <span className="serviceRollUpCategoryLabel">{category}</span>
-                </div>
-                <span className="labels">{service}</span>
-                <span className="labels">{price}</span>
-            </div>
 
+        <>
+            <div className="Outer my-1">
+                <div className="banner py-0 my-0 gy-0">
+                    123456789
+                </div>
+                <div className="quick-example">
+                    <div className="expandable">
+                    
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corrupti,
+                            sint distinctio deserunt vel unde consequatur sequi nobis necessitatibus
+                            quis ad officiis doloremque ab, blanditiis facere possimus obcaecati
+                            voluptate sed!
+                    
+                    </div>
+                </div>
+            </div>
         </>
+
+
     );
-}
+};
+
+{/* <div className="serviceRollUp">
+
+<div className="serviceRollUpCategoryLabelWrapper">
+    <span className="serviceRollUpCategoryLabel">{category}</span>
+</div>
+<span className="serviceRollUpServiceLabel">{service}</span>
+<span className="serviceRollUpPriceLabel">Price: {price}€</span>
+<span className="">
+    <button
+        className="reviewServiceButton ms-auto"
+        type="button"
+    >
+        review and schedule
+    </button>
+</span>
+
+</div> */}
