be3f6a06e5dabe6fe51acd6d76f12892e677df16
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-06-15 19:06:27 +0000

    modal 15/06/2023

--- src/front/js/component/calendarModal.js
+++ src/front/js/component/calendarModal.js
@@ -205,9 +205,6 @@ export const CalendarModal = (props) => {
                     <span className="pickerValue me-1">{newServiceRequest.quantity}</span>
                     <span className="pickerControls me-1"><button onClick={handleIncreaseQuantity}>+</button></span>
                     <span className="pickerControls"><button onClick={handleDecreaseQuantity}>-</button></span>
-                    {/*   <div className="subtotal">
-                        <span>subtotal:</span><span>{price * newServiceRequest.quantity}.00€</span>
-                    </div> */}
                 </div>
             </>
         );
@@ -273,22 +270,23 @@ export const CalendarModal = (props) => {
         return (
             <>
                 <div className="address-buttons">
-                    {store.userAddresses && store.userAddresses.id1 && (
-                        <button
-                            className={`address-button ${newServiceRequest.address_id === store.userAddresses.id1 ? 'activeAddressButton' : 'inactiveAddressButton'}`}
-                            onClick={() => handleAddressChange(store.userAddresses.id1)}
-                        >
-                            main
-                        </button>
-                    )}
-                    {store.userAddresses && store.userAddresses.id2 && (
-                        <button
-                            className={`address-button ${newServiceRequest.address_id === store.userAddresses.id2 ? 'activeAddressButton' : 'inactiveAddressButton'}`}
-                            onClick={() => handleAddressChange(store.userAddresses.id2)}
-                        >
-                            second
-                        </button>
-                    )}
+                        
+                        {store.userAddresses && store.userAddresses.id1 && (
+                            <button
+                                className={`address-button ${newServiceRequest.address_id === store.userAddresses.id1 ? 'activeAddressButton' : 'inactiveAddressButton'}`}
+                                onClick={() => handleAddressChange(store.userAddresses.id1)}
+                            >
+                                main
+                            </button>
+                        )}
+                        {store.userAddresses && store.userAddresses.id2 && (
+                            <button
+                                className={`address-button ${newServiceRequest.address_id === store.userAddresses.id2 ? 'activeAddressButton' : 'inactiveAddressButton'}`}
+                                onClick={() => handleAddressChange(store.userAddresses.id2)}
+                            >
+                                second
+                            </button>
+                        )}
                 </div>
             </>
         );
@@ -300,12 +298,12 @@ export const CalendarModal = (props) => {
         <>
             <dialog data-modal id={"dialog" + id} className="">
                 <div className="calendarModalWrapper">
-                    <div className=" d-flex justify-content-center align-items-center">
+                    {/* <div className=" d-flex justify-content-center align-items-center">
                         <span className="modalTitle">book: {service.toLowerCase()}</span>
-                    </div>
+                    </div> */}
                     <div>
                         <div className="modalSubTitle d-flex justify-content-center mb-1">
-                            <span>
+                            <span className="me-1">
                                 <svg className="p-0 m-0 g-0" xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M180-80q-24 0-42-18t-18-42v-620q0-24 18-42t42-18h65v-60h65v60h340v-60h65v60h65q24 0 42 18t18 42v620q0 24-18 42t-42 18H180Zm0-60h600v-430H180v430Zm0-490h600v-130H180v130Zm0 0v-130 130Zm300 230q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z" /></svg>
                             </span>
                             <span className="modalOptionTiltle m-0 ps-1 g-0 d-flex justify-content-start">
@@ -316,39 +314,43 @@ export const CalendarModal = (props) => {
                             <Calendar />
                         </div>
                     </div>
-                    <div>
-                        <div className="modalSubTitle d-flex justify-content-center mt-1 mb-1">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="m627-287 45-45-159-160v-201h-60v225l174 181ZM480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-82 31.5-155t86-127.5Q252-817 325-848.5T480-880q82 0 155 31.5t127.5 86Q817-708 848.5-635T880-480q0 82-31.5 155t-86 127.5Q708-143 635-111.5T480-80Zm0-400Zm0 340q140 0 240-100t100-240q0-140-100-240T480-820q-140 0-240 100T140-480q0 140 100 240t240 100Z" /></svg>
-                            </span>
-                            <span className="modalOptionTiltle m-0 ps-1 g-0 d-flex justify-content-start">
-                                Time
-                            </span>
+                    <div className="d-flex flex-row justify-content-around mt-1">
+                        <div>
+                            <div className="modalSubTitle d-flex justify-content-center">
+                                <span className="me-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="m627-287 45-45-159-160v-201h-60v225l174 181ZM480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-82 31.5-155t86-127.5Q252-817 325-848.5T480-880q82 0 155 31.5t127.5 86Q817-708 848.5-635T880-480q0 82-31.5 155t-86 127.5Q708-143 635-111.5T480-80Zm0-400Zm0 340q140 0 240-100t100-240q0-140-100-240T480-820q-140 0-240 100T140-480q0 140 100 240t240 100Z" /></svg>
+                                </span>
+                                <span className="modalOptionTiltle d-flex justify-content-start">
+                                    Time
+                                </span>
+                            </div>
+                            <div className="calendarContainer">
+                                <HourPicker />
+                            </div>
                         </div>
-                        <div className="calendarContainer">
-                            <HourPicker />
+                        <div>
+                            <div className="modalSubTitle d-flex justify-content-center">
+                                <span className="me-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M286.788-81Q257-81 236-102.212q-21-21.213-21-51Q215-183 236.212-204q21.213-21 51-21Q317-225 338-203.788q21 21.213 21 51Q359-123 337.788-102q-21.213 21-51 21Zm400 0Q657-81 636-102.212q-21-21.213-21-51Q615-183 636.212-204q21.213-21 51-21Q717-225 738-203.788q21 21.213 21 51Q759-123 737.788-102q-21.213 21-51 21ZM235-741l110 228h288l125-228H235Zm-30-60h589.074q22.964 0 34.945 21Q841-759 829-738L694-495q-11 19-28.559 30.5Q647.881-453 627-453H324l-56 104h491v60H277q-42 0-60.5-28t.5-63l64-118-152-322H51v-60h117l37 79Zm140 288h288-288Z" /></svg>
+                                </span>
+                                <span className="modalOptionTiltle d-flex justify-content-start">
+                                    Quantity
+                                </span>
+                            </div>
+                            <div className="calendarContainer">
+                                <QuantityPicker />
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <div className="modalSubTitle d-flex justify-content-center mt-1 mb-1">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M286.788-81Q257-81 236-102.212q-21-21.213-21-51Q215-183 236.212-204q21.213-21 51-21Q317-225 338-203.788q21 21.213 21 51Q359-123 337.788-102q-21.213 21-51 21Zm400 0Q657-81 636-102.212q-21-21.213-21-51Q615-183 636.212-204q21.213-21 51-21Q717-225 738-203.788q21 21.213 21 51Q759-123 737.788-102q-21.213 21-51 21ZM235-741l110 228h288l125-228H235Zm-30-60h589.074q22.964 0 34.945 21Q841-759 829-738L694-495q-11 19-28.559 30.5Q647.881-453 627-453H324l-56 104h491v60H277q-42 0-60.5-28t.5-63l64-118-152-322H51v-60h117l37 79Zm140 288h288-288Z" /></svg>
-                            </span>
-                            <span className="modalOptionTiltle m-0 ps-1 g-0 d-flex justify-content-start">
-                                Quantity
-                            </span>
-                        </div>
-                        <div className="calendarContainer">
-                            <QuantityPicker />
-                        </div>
-                    </div>
+
+
                     <div>
                         <div className="modalSubTitle d-flex justify-content-center mt-1 mb-1">
                             <span>
                                 <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M480-80q-75 0-140.5-28T225-185q-49-49-77-114.5T120-440h60q0 125 87.5 212.5T480-140q125 0 212.5-87.5T780-440q0-125-85-212.5T485-740h-23l73 73-41 42-147-147 147-147 41 41-78 78h23q75 0 140.5 28T735-695q49 49 77 114.5T840-440q0 75-28 140.5T735-185q-49 49-114.5 77T480-80Z" /></svg>
                             </span>
                             <span className="modalOptionTiltle m-0 ps-1 g-0 d-flex justify-content-start">
-                                Recurrence
+                                Repeat
                             </span>
                         </div>
                         <div className="calendarContainer">
@@ -356,12 +358,12 @@ export const CalendarModal = (props) => {
                         </div>
                     </div>
                     <div className="modalSubTitle d-flex justify-content-center mt-1 mb-1">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M220-180h150v-250h220v250h150v-390L480-765 220-570v390Zm-60 60v-480l320-240 320 240v480H530v-250H430v250H160Zm320-353Z"/></svg>
-                            </span>
-                            <span className="modalOptionTiltle m-0 ps-1 g-0 d-flex justify-content-start">
-                                Address
-                            </span>
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M220-180h150v-250h220v250h150v-390L480-765 220-570v390Zm-60 60v-480l320-240 320 240v480H530v-250H430v250H160Zm320-353Z" /></svg>
+                        </span>
+                        <span className="modalOptionTiltle m-0 ps-1 g-0 d-flex justify-content-start">
+                            Address
+                        </span>
                         <div className="calendarContainer">
                             <AddressPicker />
                         </div>
@@ -417,3 +419,7 @@ export const CalendarModal = (props) => {
                 <span>
                     * 9: {newServiceRequest.address_id} *
                 </span> */}
+
+{/*   <div className="subtotal">
+                        <span>subtotal:</span><span>{price * newServiceRequest.quantity}.00€</span>
+                    </div> */}
